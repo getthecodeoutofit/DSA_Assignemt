43a22ca8072fc1869daee9cd6083c982d08e9b66
Ravi Mahar <ravimahar12321@gmail.com>
2023-09-25 17:29:12 +0530

    "2"

--- QS_2.c
+++ QS_2.c
@@ -2,18 +2,33 @@
 #include<stdio.h>
 
 int main(){
-    int arr1[50],arr2[50],un[50],i,j,count,k,n1,n2;
+    int arr1[50],arr2[50],un[50],i,j,n1,n2,count=0,n3,k;
     printf("Enter the number of elements in the array 1: ");
     scanf("%d",&n1);
     printf("Enter the number of elements in the array 2: ");
     scanf("%d",&n2);
     for(i=0;i<n1;i++){
         scanf("%d",&arr1[i]);
-        
+        un[i] = arr1[i];
     }
+    n3=n1;
     for(j=0;j<n2;j++){
-        scanf("%d",&arr2[j]);    
+        scanf("%d",&arr2[j]);
+        for(k=0;k<n3;k++){
+            if(un[k]==arr2[j]){
+                count++;
+            }
+        }
+        if(count==0){
+            un[n3++]=arr2[j];    
+        } 
+        count=0;
     }
+    printf("The union is: ");
+    for(i=0;i<n3;i++){
+        printf("%d ",un[i]);
+    }
+    printf("\n");
 
     return 0;
 }
